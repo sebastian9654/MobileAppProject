9602d051db7df9f24c74fedc11f6b90bfe53dc62
sebastian9654 <rodriguezseba2001@gmail.com>
2023-11-27 01:15:40 -0500

    added yesenias' recent changes

---
+++ MobileAppProject-main/app/src/main/java/com/example/project1/SettingsActivity.java
@@ -0,0 +1,25 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+        // below line is used to check if
+        // frame layout is empty or not.
+        if (findViewById(R.id.idFrameLayout) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            // below line is to inflate our fragment.
+            getFragmentManager().beginTransaction().add(R.id.idFrameLayout, new SettingFragment()).commit();
+        }
+    }
+}
