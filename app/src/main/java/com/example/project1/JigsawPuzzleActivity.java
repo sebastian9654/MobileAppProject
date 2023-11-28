9cab477ddf23a1e207f91d1e8920ba91c9053a58
sebastian9654 <rodriguezseba2001@gmail.com>
2023-11-28 15:03:04 -0500

    Added more functionality to JigsawPuzzleActivity

--- app/src/main/java/com/example/project1/JigsawPuzzleActivity.java
+++ app/src/main/java/com/example/project1/JigsawPuzzleActivity.java
@@ -3,11 +3,11 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.os.CountDownTimer;
-import org.w3c.dom.Text;
-
 import java.util.Random;
 
 public class JigsawPuzzleActivity extends AppCompatActivity {
@@ -15,21 +15,53 @@ public class JigsawPuzzleActivity extends AppCompatActivity {
     private Random random = new Random();
     private TextView timeView;
     public boolean levelCompleted = false;
+    public boolean gameActive = false;
     private ImageView puzzleImageView;
+    private Button startJigsawGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jigsaw_puzzle);
 
+        this.gameActive = true;
+
+        timeView = findViewById(R.id.timerView);
         puzzleImageView = findViewById(R.id.puzzleImageView);
 
         // Choose a random image from our ingredients array to be the puzzle
         int randomIngredientIndex = random.nextInt(ingredients.length);
         int selectedIngredient = ingredients[randomIngredientIndex];
+
         puzzleImageView.setImageResource(selectedIngredient);
+        startJigsawGame = findViewById(R.id.startJigsawGame);
 
-        timeView = findViewById(R.id.timerView);
+        startJigsawGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLevel();
+            }
+        });
+    }
 
+    private void startLevel() {;
+        timeView.setText("Time Remaining: 60");
+        new CountDownTimer(60000, 1000) { // count from a minute
+            public void onTick(long millisUntilFinished) {
+                // Update the timer on each tick
+                timeView.setText("Time Remaining: " + millisUntilFinished / 1000);
+            }
+            @Override
+            public void onFinish() {
+                levelCompleted = false; // keep as false since time limit ran out.
+                endLevel();
+            }
+
+        }.start();
+    }
+
+    private void endLevel() {
+        this.gameActive = false;
+        finish();
     }
 }
