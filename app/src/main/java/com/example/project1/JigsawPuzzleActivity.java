4eb7b7a603cdd830dc19c79d8eb5f847415e093d
sebastian9654 <rodriguezseba2001@gmail.com>
2023-12-02 16:36:42 -0500

    Changed the layout of Jigsaw Puzzle

--- app/src/main/java/com/example/project1/JigsawPuzzleActivity.java
+++ app/src/main/java/com/example/project1/JigsawPuzzleActivity.java
@@ -1,7 +1,6 @@
 package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -11,8 +10,6 @@ import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.os.CountDownTimer;
-
-import java.util.Arrays;
 import java.util.Random;
 
 public class JigsawPuzzleActivity extends AppCompatActivity {
@@ -24,18 +21,17 @@ public class JigsawPuzzleActivity extends AppCompatActivity {
     private ImageView puzzleImageView;
     private Button startJigsawGame;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jigsaw_puzzle);
 
         this.gameActive = true;
-
         timeView = findViewById(R.id.timerView);
+
         puzzleImageView = findViewById(R.id.puzzleImageView);
 
-        // Initially display the original ingredient image
+        // Set the image resource with your selected ingredient
         int randomIngredientIndex = random.nextInt(ingredients.length);
         final int selectedIngredient = ingredients[randomIngredientIndex];
         puzzleImageView.setImageResource(selectedIngredient);
@@ -47,9 +43,8 @@ public class JigsawPuzzleActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startLevel();
-                startJigsawGame.setVisibility(View.INVISIBLE);
+                startJigsawGame.setVisibility(View.VISIBLE);
 
-                // Find the index of the displayed ingredient in the ingredients array
                 int currentIngredientIndex = -1;
                 for (int i = 0; i < ingredients.length; i++) {
                     if (ingredients[i] == selectedIngredient) {
@@ -59,33 +54,29 @@ public class JigsawPuzzleActivity extends AppCompatActivity {
                 }
 
                 if (currentIngredientIndex != -1 && currentIngredientIndex < jigsawVersions.length) {
-                    // Replace the original ingredient with its jigsaw version
                     int jigsawVersion = jigsawVersions[currentIngredientIndex];
                     puzzleImageView.setImageResource(jigsawVersion);
 
-                    // Wait a bit before splitting the image into pieces to ensure it's properly set
                     puzzleImageView.postDelayed(new Runnable() {
                         @Override
                         public void run() {
                             splitImageIntoPieces();
                         }
-                    }, 100); // Adjust the delay time as needed
+                    }, 100);
                 }
             }
         });
 
-        // Hide the button initially until the user clicks "Start Level"
+        // Show the button initially until the user clicks "Start Level"
         startJigsawGame.setVisibility(View.VISIBLE);
     }
 
     private void splitImageIntoPieces() {
-        // Split the image into 9 pieces
         BitmapDrawable drawable = (BitmapDrawable) puzzleImageView.getDrawable();
         Bitmap bitmap = drawable.getBitmap();
         int pieceWidth = bitmap.getWidth() / 3;
         int pieceHeight = bitmap.getHeight() / 3;
 
-        // Create a 3x3 grid of ImageViews to hold the puzzle pieces
         ImageView[][] puzzlePieces = new ImageView[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -95,12 +86,10 @@ public class JigsawPuzzleActivity extends AppCompatActivity {
             }
         }
 
-        // Shuffle the puzzle pieces
         shufflePieces(puzzlePieces);
 
-        // Display the shuffled puzzle pieces in a 3x3 grid
         GridLayout puzzleGridLayout = findViewById(R.id.puzzleGridLayout);
-        puzzleGridLayout.removeAllViews(); // Clear any existing views in the layout
+        puzzleGridLayout.removeAllViews();
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -109,21 +98,21 @@ public class JigsawPuzzleActivity extends AppCompatActivity {
         }
     }
 
-    private void startLevel() {;
+    private void startLevel() {
         timeView.setText("Time Remaining: 60");
-        new CountDownTimer(60000, 1000) { // count from a minute
+        new CountDownTimer(60000, 1000) {
             public void onTick(long millisUntilFinished) {
-                // Update the timer on each tick
                 timeView.setText("Time Remaining: " + millisUntilFinished / 1000);
             }
             @Override
             public void onFinish() {
-                levelCompleted = false; // keep as false since time limit ran out.
+                levelCompleted = false;
                 endLevel();
             }
 
         }.start();
     }
+
     private void shufflePieces(ImageView[][] puzzlePieces) {
         int rows = puzzlePieces.length;
         int cols = puzzlePieces[0].length;
@@ -134,7 +123,6 @@ public class JigsawPuzzleActivity extends AppCompatActivity {
                 int m = random.nextInt(i + 1);
                 int n = random.nextInt(j + 1);
 
-                // Swap puzzle pieces at (i, j) and (m, n)
                 ImageView temp = puzzlePieces[i][j];
                 puzzlePieces[i][j] = puzzlePieces[m][n];
                 puzzlePieces[m][n] = temp;
